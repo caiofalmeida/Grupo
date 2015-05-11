7b295cb27aa40d20a37e8ac14817d471fb4281ef
Nedilson <italonedilson@gmail.com>
2015-05-11 15:47:26 -0300

    Update Motor_variavel.java

--- motor_variavel/src/motor_variavel/Motor_variavel.java
+++ motor_variavel/src/motor_variavel/Motor_variavel.java
@@ -1,9 +1,29 @@
 package motor_variavel;
-//
+
 public class Motor_variavel {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        String cod = JOptionPane.showInputDialog(null, "Código:");
+        while(!cod.equalsIgnoreCase("s")){
+            boolean res = declaracao(cod);
+            System.out.println("Resultado:" +res);
+            cod = JOptionPane.showInputDialog(null, "Código:");
+        }       
     }
-    
+    public static String nome (){
+        return "<n>";
+    }
+    public static String expressao (){
+        return "<e>";
+    }
+    public static boolean declaracao (String codigo){
+        //var <n> = <e>
+        //var +<n>|var +<n> *= *<e>
+        return codigo.matches(" *var +"+nome()+"|var "+nome()+" *= *"+expressao());
+    }
+    public static boolean atribuicao (String codigo){
+        //<n> = <e>
+        // *<n> *= *<e>
+        return codigo.matches(" *"+nome()+" *= *"+expressao());
+    }
 }
